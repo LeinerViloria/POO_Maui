cc27e3191880d8dd33d34d3deb693b4ce1a0e720
LeinerViloria <vilorialeiner2002@gmail.com>
2024-02-11 12:48:02 -0500

    Ajustes

--- Prueba/MainPage.xaml.cs
+++ Prueba/MainPage.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace Prueba
+﻿using System.Collections;
+using DevExpress.Data.Browsing;
+
+namespace Prueba
 {
     public partial class MainPage : ContentPage
     {
+        IList Items {get; set;}
+
         public MainPage()
         {
+            Items = new List<string>() { "Elemento 1", "Elemento 2", "Elemento 3" };
             InitializeComponent();
+            ComboBox.Data = Items;
         }
     }
 
